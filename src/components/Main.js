a87f7d73469719afe81859406c78296c537c7f03
Dmitry Lyalin <lyalin.dima@gmail.com>
2016-11-01 20:44:56 +0300

    Add Header and Form components

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,13 @@
 import React from 'react';
+import Header from '../components/Header'
 import '../styles/index.css';
-import '../styles/Main.css';
 
 class Main extends React.Component {
   render() {
     return (
-      <div className="Main">
+      <div>
 
-        <div className="Main-header">
-
-          <h2 className="Main-header__title">Open Weather App</h2>
-
-          <form className="Main-header__form">
-            <input className="form-input" type="text" placeholder="Moscow, Russia"/>
-            <button className="form-button" type="button">Get Weather</button>
-          </form>
-
-        </div>
+        <Header/>
 
         {this.props.children}
 
