2f7cb6d3ed083518a58d7191cb379067e51f9e7f
Dmitry Lyalin <lyalin.dima@gmail.com>
2016-11-05 16:21:21 +0300

    Add console.log

--- src/components/ForecastBase.js
+++ src/components/ForecastBase.js
@@ -3,6 +3,8 @@ import Loading from './Loading'
 
 class ForecastBase extends React.Component {
   render() {
+    console.log('FROM FORECAST_BASE.JS:');
+    console.log(this.props.location.state.weather['city']);
     return (
       <div>
         <Loading/>
