66cda2b2c5862a8c92ac36e29f6b58771c3f84ec
Dmitry Lyalin <lyalin.dima@gmail.com>
2016-11-05 16:12:18 +0300

    Add loading animation and new route to second page

---
+++ src/components/ForecastBase.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Loading from './Loading'
+
+class ForecastBase extends React.Component {
+  render() {
+    return (
+      <div>
+        <Loading/>
+      </div>
+    );
+  }
+}
+
+export default ForecastBase;
--- src/config/routes.js
+++ src/config/routes.js
@@ -2,11 +2,13 @@ import React from 'react'
 import {Router, Route, IndexRoute, hashHistory} from 'react-router'
 import Main from '../components/Main'
 import Home from '../components/Home'
+import ForecastBase from '../components/ForecastBase'
 
 const routes = (
   <Router history={hashHistory}>
     <Route path='/' component={Main}>
       <IndexRoute component={Home}/>
+      <Route path='/forecast/:city' component={ForecastBase}/>
     </Route>
   </Router>
 );
