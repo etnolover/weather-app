815e8e22e17532a425b460788060fa946f4bd4fa
Dmitry Lyalin <lyalin.dima@gmail.com>
2016-11-17 20:55:48 +0300

    Change Route component from ForecastBase to ForecastBaseContainer

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,13 +2,13 @@ import React from 'react'
 import {Router, Route, IndexRoute, hashHistory} from 'react-router'
 import Main from '../components/Main'
 import Home from '../components/Home'
-import ForecastBase from '../components/ForecastBase'
+import ForecastBaseContainer from '../components/ForecastBaseContainer'
 
 const routes = (
   <Router history={hashHistory}>
     <Route path='/' component={Main}>
       <IndexRoute component={Home}/>
-      <Route path='/forecast/:city' component={ForecastBase}/>
+      <Route path='/forecast/:city' component={ForecastBaseContainer}/>
     </Route>
   </Router>
 );
