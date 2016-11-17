5fe30de612a916496db5d862dc103723f4d2212e
Dmitry Lyalin <lyalin.dima@gmail.com>
2016-11-17 20:25:31 +0300

    Add container with logic for displaying weather data

---
+++ src/components/ForecastBaseContainer.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import ForecastBase from '../components/ForecastBase'
+import weatherHelpers from '../helpers/weatherApi'
+
+class ForecastBaseContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weatherLocation: props.location.state.weatherLocation,
+      weatherData: {},
+      isLoading: true
+    };
+  }
+
+  componentDidMount() {
+    weatherHelpers.getSevenDayWeatherForecast(this.state.weatherLocation)
+      .then((data) => {
+        console.log(`FROM FBContainer: City: ${data.city}, Temperature: ${data.temp}`);
+        this.setState({
+          weatherData: data,
+          isLoading: false
+        })
+      })
+  }
+
+  render() {
+    return (
+      <ForecastBase
+        weatherData={this.state.weatherData}
+        isLoading={this.state.isLoading}/>
+    );
+  };
+
+}
+
+export default ForecastBaseContainer;
