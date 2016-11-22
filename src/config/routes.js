77d7ac3c4b8fef3de310dbca84fff0080a348bbd
Dmitry Lyalin <lyalin.dima@gmail.com>
2016-11-22 13:31:01 +0300

    Change hashHistory to browserHistory

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import {Router, Route, IndexRoute, hashHistory} from 'react-router'
+import {Router, Route, IndexRoute, browserHistory} from 'react-router'
 import Main from '../components/Main'
 import Home from '../components/Home'
 import ForecastBaseContainer from '../containers/ForecastBaseContainer'
 
 const routes = (
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path='/' component={Main}>
       <IndexRoute component={Home}/>
       <Route path='/forecast/:city' component={ForecastBaseContainer}/>
